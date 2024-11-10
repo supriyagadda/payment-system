96c164eafc67ced6b2fef04ef879f99056984652
Supriya <gaddasupriya@gmail.com>
2024-11-10 11:54:32 -0500

    Card changes

--- payment-systems/src/main/java/com/payments/systems/exception/PaymentSystemsControllerAdvise.java
+++ payment-systems/src/main/java/com/payments/systems/exception/PaymentSystemsControllerAdvise.java
@@ -4,8 +4,13 @@ package com.payments.systems.exception;
 import com.payments.systems.model.OutputMetaData;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class PaymentSystemsControllerAdvise {
@@ -13,6 +18,16 @@ public class PaymentSystemsControllerAdvise {
     @ExceptionHandler(BaseException.class)
     public ResponseEntity<OutputMetaData> handleBaseException(BaseException ex){
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getOutputMetaData());
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
 
