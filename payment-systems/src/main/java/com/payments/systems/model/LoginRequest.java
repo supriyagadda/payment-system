37768a56d741ee7d579cefe644945ec2352580f0
Supriya <gaddasupriya@gmail.com>
2024-11-10 11:48:37 -0500

    Card changes

--- payment-systems/src/main/java/com/payments/systems/model/LoginRequest.java
+++ payment-systems/src/main/java/com/payments/systems/model/LoginRequest.java
@@ -1,6 +1,7 @@
 package com.payments.systems.model;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -11,7 +12,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class LoginRequest {
     @NotNull(message = "emailid is required.")
+    @NotEmpty (message = "Emaild should be provided.")
     private String emailid;
     @NotNull(message = "password is required.")
+    @NotEmpty(message = "password should be there.")
     private String password;
 }
