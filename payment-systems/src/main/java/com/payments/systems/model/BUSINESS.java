b1d10408349b3b23138e8890c9c981a70685a8fc
Supriya <gaddasupriya@gmail.com>
2024-11-12 12:12:25 -0500

    Business enum

---
+++ payment-systems/src/main/java/com/payments/systems/model/BUSINESS.java
@@ -0,0 +1,13 @@
+package com.payments.systems.model;
+
+public enum BUSINESS {
+
+    ELECTRICITY,
+    WIFI,
+    MOBILE,
+    CREDITCARD,
+    CARINSURANCE,
+    PROPERTYTAX,
+    DONATION,
+    GAS
+}
